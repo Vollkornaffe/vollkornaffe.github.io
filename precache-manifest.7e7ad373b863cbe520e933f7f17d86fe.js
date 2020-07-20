c4f27be978aa24e1e9e5d919a17f72de81f9301f
Lars Helge Scheel <larshelge.scheel@gmail.com>
2020-07-20 13:50:11 +0200

    Updates

---
+++ precache-manifest.7e7ad373b863cbe520e933f7f17d86fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ddba15b781c46575f9eb9a0f1edf44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5582972e8235547045bf",
+    "url": "/static/css/main.f9e4b7cb.chunk.css"
+  },
+  {
+    "revision": "954d3e3f8f65c8d31862",
+    "url": "/static/js/2.03008f52.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.03008f52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5582972e8235547045bf",
+    "url": "/static/js/main.e3b90d1e.chunk.js"
+  },
+  {
+    "revision": "1231b590de0c6e66d2d1",
+    "url": "/static/js/runtime-main.428fc331.js"
+  }
+]);
